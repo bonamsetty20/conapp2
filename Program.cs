d9d594dd3e0a106d7442504ff79bb459e02384c9
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-07-11 11:11:09 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name;
+            char gender;
+            double salary;
+            DateTime DOJ;
+
+            Console.WriteLine("Enter id");
+            id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter gender");
+            gender = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter salary");
+            salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter date of joining");
+            DOJ = DateTime.Parse(Console.ReadLine());
+            if (salary > 90000)
+            {
+                Console.WriteLine("You have to pay 30% tax");
+            }
+            else
+            {
+                Console.WriteLine("Tax will be 15%");
+            }
+
+            Console.WriteLine("ID : \t" + id + "\n NAME: " + name + "\n Gender: \t " + gender + "\n salary: \t  " + salary + "\n Date of joining: \t " + DOJ);
+            Console.ReadKey();
+
+
+        }
+    }
+}
